6fc0643459ec134617cee0416c1c4a3b39bed5bf
unknown <natalishapov@live.com>
2017-10-03 13:59:46 +0300

    skills js

--- app/js/js-modules/main.js
+++ app/js/js-modules/main.js
@@ -1,5 +1,25 @@
-//= js-assets/variables
 //= js-assets/functions
+
+$(document).ready(function() {
+	$(window).on('scroll', function(event) {
+		var skillsBlock = $('#skillsBlock');
+
+		if(skillsBlock.length) {
+			if($(this).scrollTop >= skillsBlock.offset().top) {
+
+			$('.skills-item').each(function(index, el) {
+
+				var dataSkill = $(this).data('skill');
+
+				$(el).find('.percentage').text(dataSkill);
+				$(el).find('.item-bar__indicator').animate({
+					width: dataSkill + '%'
+				}, 1000);	
+			});
+		}
+		}
+	});
+});
 
 
 
